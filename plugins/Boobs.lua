221c9035b057712d183587f40b5c6068981789a6
gmorishere <aryan.shahin@icloud.com>
2016-01-21 16:34:58 +0330

    Update Boobs.lua

--- plugins/Boobs.lua
+++ plugins/Boobs.lua
@@ -40,11 +40,11 @@ end
 local function run(msg, matches)
   local url = nil
   
-  if matches[1] == "!boobs" then
+  if matches[1] == "boobs" then
     url = getRandomBoobs()
   end
 
-  if matches[1] == "!butts" then
+  if matches[1] == "butts" then
     url = getRandomButts()
   end
 
@@ -63,8 +63,8 @@ return {
     "!butts: Get a butts NSFW image. ًں”‍"
   },
   patterns = {
-    "^!boobs$",
-    "^!butts$"
+    "^[/!](boobs)$",
+    "^[!/](butts)$"
   }, 
   run = run 
 }
